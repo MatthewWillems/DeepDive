0aa011ed30171407eb3cb22c94aa09b913c7c5bf
Daniel <33257@ma-web.nl>
2023-04-01 14:55:30 +0200

    added js

--- stressSchema/js/main.js
+++ stressSchema/js/main.js
@@ -1,6 +1,33 @@
-class popup{
+class Card {
+  constructor(element, footer) {
+    this.element = element;
+    this.footer = footer;
+    this.addClickListener();
+  }
 
-    constructor() {
-        
-    }
+  addClickListener() {
+    this.element.addEventListener('click', () => {
+      this.footer.style.display = 'inline';
+    });
+  }
 }
+
+const cards = document.querySelectorAll("#js--cards");
+
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+    // Hide all other cards
+    cards.forEach(otherCard => {
+      if (otherCard !== card) {
+        otherCard.style.display = "none";
+      }
+    });
+
+    // Show the footer
+    const footer = document.getElementById("js--footer");
+    footer.style.display = "inline";
+  });
+});
+
+
+
